f3a9176e26b01d8c3b8b98d8fd3b9dc87a30eb8a
dhinesh256 <76810193+dhinesh256@users.noreply.github.com>
2021-08-31 11:55:10 +0530

    Add files via upload

---
+++ Javascript/Events/basics.js
@@ -0,0 +1,16 @@
+let message=function(){
+    //alert("hello world");
+    let a=2
+    document.getElementById("one").innerHTML=`${(a.toString().bold())}`
+}
+
+let mymap=new Map()
+mymap.set("make","BMW")
+mymap.set("price",12333)
+console.log(mymap);
+
+
+let myset=new Set()
+myset.add("hello").add("world")
+console.log(myset);
+
