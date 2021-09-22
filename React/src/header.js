a58035fd80f10bd875075545e62e6c4be08d1faa
Dhinesh <76810193+dhinesh256@users.noreply.github.com>
2021-09-22 20:34:54 +0530

    Add files via upload

---
+++ React/src/header.js
@@ -0,0 +1,13 @@
+//import { render } from "@testing-library/react";
+import React from "react";
+//import ReactDom from "react-dom";
+
+
+
+class header extends React.Component{
+    render() {
+    return <div className="App">Hello</div>
+  }
+}
+  
+  export default header;
