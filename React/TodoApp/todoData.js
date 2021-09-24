cb6316439416a2242a2fa14f7960d693f49380fd
Dhinesh <76810193+dhinesh256@users.noreply.github.com>
2021-09-24 20:30:46 +0530

    Add files via upload

---
+++ React/TodoApp/todoData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id: 1,
+        text: "JavaScript",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "HTML and CSS",
+        completed: true
+    },
+    {
+        id: 3,
+        text: "React",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "React Native",
+        completed: false
+    },
+    {
+        id: 5,
+        text: "Project",
+        completed: false
+    }
+]
+
+export default todosData
