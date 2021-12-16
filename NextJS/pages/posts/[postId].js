8f9825ff8c9d17a4550390c5a92bdd569156af1f
Dhinesh <76810193+dhinesh256@users.noreply.github.com>
2021-12-16 20:11:38 +0530

    Add files via upload

--- NextJS/pages/posts/[postId].js
+++ NextJS/pages/posts/[postId].js
@@ -1,4 +1,13 @@
+import { useRouter } from "next/router"
+
 const PostId = ({posts}) => {
+
+    const router = useRouter()
+
+    if(router.isFallback){
+        return <h1>Loading....</h1>
+    }
+
     return (
     <div>
         <h1> {posts.title}</h1>
@@ -9,19 +18,35 @@ const PostId = ({posts}) => {
 export default PostId
 
 export async function getStaticPaths(){
-    const posts = await fetch(`http://jsonplaceholder.typicode.com/posts`)
-    const data = await posts.json()
-    const paths = data.map((path) =>{
-        return {
-            params:{
-                postId:`${path.id}`,
-            }
-        }
-    })
+
+    // const posts = await fetch(`http://jsonplaceholder.typicode.com/posts`)
+    // const data = await posts.json()
+    // const paths = data.map((path) =>{
+    //     return {
+    //         params:{
+    //             postId:`${path.id}`,
+    //         }
+    //     }
+    // })
 
     return {
-    paths,
-    fallback:false}
+        paths: [
+            {
+                params : { postId : '1'},
+            },
+            {
+                params : { postId : '2'},
+            },
+            {
+                params : { postId : '3'},
+            },
+        ],
+        fallback:true
+    }
+
+    // return {
+    // paths,
+    // fallback:false}
 }
 
 export async function getStaticProps(context){
